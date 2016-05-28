bc66b08aa750641f4cb1785e7a91a1929a0f5da7
Matthew Campbell <ematthewcampbell@gmail.com>
2016-05-28 15:21:54 -0600

    Created iteration one

--- test/server_test.rb
+++ test/server_test.rb
@@ -5,4 +5,10 @@ require "pry"
 
 class ServerTest < Minitest::Test
 
+def rips_apart_requests
+  server = Server.new
+  request = ["POST / HTTP/1.1", "Host: 127.0.0.1:9292", "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,​*/*​;q=0.8", "Accept-Language: en-us", "Connection: keep-alive", "Accept-Encoding: gzip, deflate", "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17"]
+  output = "<pre>\nVerb: POST\nPath: /\nProtocol: HTTP/1.1\nHost: 127.0.0.1\nPort: 9292\nOrigin: 127.0.0.1\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n</pre>"
+  assert_equal output, server.rip_request
+end
 end
