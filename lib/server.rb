7cadd9fc992cc2eb1dcede8515623c05ff0d352a
tgisg <soniagupta504@gmail.com>
2016-05-30 16:38:29 -0600

    Chrome bug

--- lib/server.rb
+++ lib/server.rb
@@ -2,77 +2,53 @@ require 'socket'
 require './lib/parser'
 require "pry"
 
-# class Server
-# attr_reader       :tcp_server, :counter
-# attr_accessor     :request_lines, :client
+class Server
+attr_reader       :tcp_server, :counter
+attr_accessor     :request_lines, :client
 
-  # def initialize
-  #   @counter = 0
-  #   @hello_counter = 0
-  #   @tcp_server    = TCPServer.new(9292) #---> possibly make this TCP.Server.new(9292).accept
-  # end
-  #
-  # def receive_request
-  #   @client = tcp_server.accept
-  #   @request_lines = []
-  #   while line = client.gets and !line.chomp.empty?
-  #     @request_lines << line.chomp
-  #   end
-  #   @request_lines
-  # end
-  #
-  # def respond_to_client
-  #   # puts @request_lines.inspect
-  #   response = "<pre>" + Parser.new(@request_lines).final_response + "</pre>"
-  #     if response == "<pre>Hello, World</pre>"
-  #       @hello_counter += 1
-  #       response = "<pre>Hello, World #{@hello_counter}</pre>"
-  #     elsif response == "<pre>Total Requests:</pre>"
-  #       response = "<pre>Total Requests: #{@counter}</pre>"
-  #       @tcp_server.close
-  #     end
-  #       output = "<html><head></head><body>#{response}</body></html>"
-  #   @client.puts output
-  # end
-  #
-  # def open_server
-  #   loop do
-  #     @counter += 1
-  #     receive_request
-  #     respond_to_client
-  #     @client.close
-  #   end
-  # end
+  def initialize
+    @counter = 0
+    @hello_counter = 0
+    @tcp_server    = TCPServer.new(9292) #---> possibly make this TCP.Server.new(9292).accept
+  end
 
-  # def combined
-    tcp_server    = TCPServer.new(9292) #---> possibly make this TCP.Server.new(9292).accept
-    counter = 0
-    hello_counter = 0
-    loop do
-      counter += 1
-      client = tcp_server.accept
-      request_lines = []
-      while line = client.gets and !line.chomp.empty?
-        request_lines << line.chomp
-      end
-      response = "<pre>" + Parser.new(request_lines).final_response + "</pre>"
+  def receive_request
+    @client = tcp_server.accept
+    @request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      @request_lines << line.chomp
+    end
+    @request_lines
+  end
+
+  def respond_to_client
+    # puts @request_lines.inspect
+    response = "<pre>" + Parser.new(@request_lines).final_response + "</pre>"
       if response == "<pre>Hello, World</pre>"
-        hello_counter += 1
-        response = "<pre>Hello, World #{hello_counter}</pre>"
+        @hello_counter += 1
+        response = "<pre>Hello, World #{@hello_counter}</pre>"
       elsif response == "<pre>Total Requests:</pre>"
-        response = "<pre>Total Requests: #{counter}</pre>"
-        tcp_server.close
+        response = "<pre>Total Requests: #{@counter}</pre>"
+        @tcp_server.close
       end
         output = "<html><head></head><body>#{response}</body></html>"
-    client.puts output
-    client.close
+    @client.puts output
+  end
+
+  def open_server
+    loop do
+      @counter += 1
+      receive_request
+      respond_to_client
+      @client.close
     end
-  # end
+  end
 
-# end
+end
 
-# server = Server.new
-# server.combined
+
+server = Server.new
+server.open_server
 
 
 # parser = Parser.new(server.receive_request)
