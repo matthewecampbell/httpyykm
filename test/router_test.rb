cdf592af4c8acd9b71be2ce2d32017cc10a835f5
Matthew Campbell <ematthewcampbell@gmail.com>
2016-05-31 11:34:34 -0600

    Fixed newlines and allowed for captials

--- test/router_test.rb
+++ test/router_test.rb
@@ -17,9 +17,28 @@ class RouterTest < Minitest::Test
     assert_equal "Hello, World", router.determine_path
   end
 
-  # def test_if_path_is_word_search
-  #   router = Router.new("/word_search?word=horses")
-  #
-  #   assert_equal "horses", router.check_dictionary
-  # end
+
+  def test_if_path_is_word_search
+    router = Router.new("/word_search?word=horse")
+
+    assert_equal "horse is a known word", router.check_dictionary
+  end
+
+  def test_cant_do_plural
+    router = Router.new("/word_search?word=horses")
+
+    assert_equal "horses is not a known word", router.check_dictionary
+  end
+
+  def test_can_do_capitals
+    router = Router.new("/word_search?word=Horse")
+    assert_equal "Horse is a known word", router.check_dictionary
+    router = Router.new("/word_search?word=HoRse")
+    assert_equal "HoRse is a known word", router.check_dictionary
+  end
+
+  def test_cant_use_made_up_word
+    router = Router.new("/word_search?word=aserwef")
+    assert_equal "aserwef is not a known word", router.check_dictionary
+  end
 end
